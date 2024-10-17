b67d79cdd6ed0431938dc9e8c398ef33f2befa56
Lucas Kruitwagen <lucas.kruitwagen@gmail.com>
2024-10-17 14:38:39 +0100

    feat: cleanup

--- tests/test_local_materialise.py
+++ tests/test_local_materialise.py
@@ -7,12 +7,12 @@ from eoflow.models import DataSpec
 def test_materialise_local():
 
     dataspec = DataSpec(
-        target_geofile="tests/data/sample.geojson",
+        target_geofile="tests/data/aoi.geojson",
     )
 
     run_cfg = {
-        "dynamic_tiles": dataspec,
-        "materialise_tile": dataspec,
+        "get_tiles_op": dataspec,
+        "dynamic_revisits": dataspec,
     }
 
     assert materialise_local.execute_in_process(run_config=RunConfig(run_cfg)).success
