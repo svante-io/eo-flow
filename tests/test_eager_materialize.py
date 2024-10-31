9e03df4db317d2328b14278a39f34debf3a1c42e
Lucas Kruitwagen <lucas.kruitwagen@gmail.com>
2024-10-31 15:42:25 +0000

    feat: test for eager materialization

---
+++ tests/test_eager_materialize.py
@@ -0,0 +1,24 @@
+from dagster import RunConfig
+
+from eoflow.dag.materialize import materialize_eager
+from eoflow.models import DataSpec
+
+
+def test_materialize_eager_extensive():
+    dataspec = DataSpec(
+        target_geofile="gs://eo-flow-public/dev/test-data/london_greenspaces.geojson",
+        dataset_store="gs://eo-flow-dev/initial_tests",
+        start_datetime="2024-10-01",
+        end_datetime="2024-10-27",
+        bands=["B04", "B03", "B02"],
+        chipsize=180,
+    )
+
+    run_cfg = {
+        "get_tiles_op": dataspec,
+        "dynamic_revisits": dataspec,
+        "op_materialize_tile_eager": dataspec,
+        "op_merge_and_store_dataset_index": dataspec,
+    }
+
+    assert materialize_eager.execute_in_process(run_config=RunConfig(run_cfg)).success
